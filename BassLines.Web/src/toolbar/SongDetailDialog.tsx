489a5373637d24281fbf4f4adb52d7da0998ae1c
Douglas Schurman <dschurman@spotinc.com>
2022-05-03 13:58:56 -0400

    redesigned search song dialog, added reviewer

--- BassLines.Web/src/toolbar/SongDetailDialog.tsx
+++ BassLines.Web/src/toolbar/SongDetailDialog.tsx
@@ -1,25 +1,15 @@
 import * as React from "react";
 import {
-  Box,
   Button,
   Dialog,
   DialogActions,
   DialogContent,
-  DialogTitle,
   Grid,
-  Typography,
+  Typography
 } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 import { SongModel } from "../data/src";
 import { format } from "date-fns";
-import { makeStyles } from "@mui/styles";
-
-const useStyles = makeStyles(() => {
-  return {
-    infoBlock: {
-      margin: "6px 0",
-    },
-  };
-});
 
 interface IProps {
   song: SongModel;
@@ -28,67 +18,85 @@ interface IProps {
 }
 
 const SongDetailDialog = (props: IProps) => {
-  const classes = useStyles();
 
   return (
     <Dialog
       fullWidth
       maxWidth="md"
       open={props.open}
-      sx={{ minHeight: "300px" }}
-    >
+      sx={{ minHeight: "300px"}}
+      >
       <DialogContent>
-        <Grid container>
-          <Grid container item xs={7}>
-            <Grid className={classes.infoBlock} item xs={12}>
+          <CloseIcon onClick={props.handleClose} sx={{cursor: "pointer", right: "24px", position: "absolute"}}/>
+          <Grid container item xs={12}>
+            <Grid item xs={4}>
+              <div style={{display: "inline-block", textAlign: "left"}}>
               <Typography variant="overline" color="inherit">Title</Typography>
-              <Typography variant="h3" color="text.primary">
+              <Typography variant="h4" color="text.primary">
                 {props.song?.title}
               </Typography>
+              </div>
             </Grid>
-            <Grid className={classes.infoBlock} item xs={12}>
+            <Grid item xs={4}>
+              <div style={{display: "inline-block", textAlign: "left"}}>
               <Typography variant="overline" color="inherit">Artist</Typography>
-              <Typography variant="h3" color="text.primary">
+              <Typography variant="h4" color="text.primary">
                 {props.song?.artist}
               </Typography>
+              </div>
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={4}>
+              <div style={{display: "inline-block", textAlign: "left"}}>
               <Typography variant="overline" color="inherit">Rating</Typography>
               <Typography
-                variant="h3"
+                variant="h4"
                 color="secondary.main"
               >{`${props.song?.rating}/10`}</Typography>
+              </div>
+            </Grid>           
+          </Grid>
+          <hr style={{marginBottom: "32px", opacity: ".33"}}/>
+          <Grid container>
+            <Grid container item xs={6}>
+              <Grid item xs={12}>
+                <div style={{display: "inline-block", textAlign: "left", marginBottom: "24px"}}>
+                <Typography variant="overline" color="inherit">Submitter</Typography>
+                <Typography color="primary.light" variant="h5">
+                  {`${props.song?.user.firstName} ${props.song?.user.lastName}`}
+                </Typography>
+                </div>
+              </Grid>
+              <Grid item xs={12}>
+                <div style={{display: "inline-block", textAlign: "left"}}>
+                <Typography variant="overline" color="inherit">Reviewer</Typography>
+                <Typography color="primary.light" variant="h5">
+                  {`${props.song?.reviewer.firstName} ${props.song?.reviewer.lastName}`}
+                </Typography>
+                </div>
+              </Grid>
+            </Grid>
+            <Grid container item xs={6}>
+              <Grid item xs={12}>
+                <div style={{display: "inline-block", textAlign: "left", marginBottom: "24px"}}>
+                <Typography variant="overline" color="inherit">Submitted</Typography>
+                <Typography color="primary.light" variant="h5">
+                  {props.song
+                    ? format(props.song?.submitteddate, "MM/dd/yyyy")
+                    : ""}
+                </Typography>
+                </div>
+              </Grid>
+              <Grid item xs={12}>
+                <div style={{display: "inline-block", textAlign: "left"}}>
+                <Typography variant="overline" color="inherit">Genre</Typography>
+                <Typography color="primary.light" variant="h5">
+                  {props.song?.genre?.name || "--"}
+                </Typography>
+                </div>
+              </Grid>
             </Grid>
           </Grid>
-          <Grid container item xs={5}>
-            <Grid className={classes.infoBlock} item xs={12}>
-              <Typography variant="overline" color="inherit">Submitter</Typography>
-              <Typography color="primary.light" variant="h4">
-                {`${props.song?.user.firstName} ${props.song?.user.lastName}`}
-              </Typography>
-            </Grid>
-            <Grid className={classes.infoBlock} item xs={12}>
-              <Typography variant="overline" color="inherit">Submitted</Typography>
-              <Typography color="primary.light" variant="h4">
-                {props.song
-                  ? format(props.song?.submitteddate, "MM/dd/yyyy")
-                  : ""}
-              </Typography>
-            </Grid>
-            <Grid className={classes.infoBlock} item xs={12}>
-              <Typography variant="overline" color="inherit">Genre</Typography>
-              <Typography color="primary.light" variant="h4">
-                {props.song?.genre?.name || "--"}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
       </DialogContent>
-      <DialogActions>
-        <Button variant="outlined" color="inherit" onClick={props.handleClose}>
-          Close
-        </Button>
-      </DialogActions>
     </Dialog>
   );
 };
