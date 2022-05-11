1cfbbba34a36d42336fbabb8b43549c5f9a87c7f
Douglas Schurman <dschurman@spotinc.com>
2022-05-11 14:54:28 -0400

    fixed user's received likes to include likes from non displayed users

--- BassLines.Api/Repositories/UserRepository.cs
+++ BassLines.Api/Repositories/UserRepository.cs
@@ -35,7 +35,11 @@ namespace BassLines.Api.Repositories
 
         public List<User> GetLeaderboardUsers()
         {
-            return _ctx.Users.Include(i => i.SongUsers).ThenInclude(i => i.Genre).Include(i => i.Likes).Where(x => x.SongUsers.Count >= 20).ToList();
+            return _ctx.Users.Include(i => i.SongUsers)
+                             .ThenInclude(s => s.Likes)
+                             .Include(i => i.SongUsers)
+                             .ThenInclude(i => i.Genre)
+                             .Where(x => x.SongUsers.Count >= 20).ToList();
         }
     }
 }
