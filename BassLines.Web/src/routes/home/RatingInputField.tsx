bcdbfcdb8029e0e0d38121ba5a6fec35de167926
Alex Johnson <ajohnson@spotinc.com>
2022-06-14 15:27:44 -0400

    refactors

--- BassLines.Web/src/routes/home/RatingInputField.tsx
+++ BassLines.Web/src/routes/home/RatingInputField.tsx
@@ -1,21 +1,27 @@
 import * as React from "react";
-import Popover from "@mui/material/Popover";
-import { Box, Button, ClickAwayListener, Divider, Grid, IconButton, Input, Paper, Stack, TextField, Tooltip, Typography } from "@mui/material";
-import { SongModel, SongsApi, UserRole } from "../../data/src";
+import {
+  Box,
+  ClickAwayListener,
+  Grid,
+  IconButton,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { SongModel, SongsApi } from "../../data/src";
 import { call } from "../../data/callWrapper";
 import { useUserState } from "../../contexts";
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
-import { useSongDispatch } from "../../contexts/songContext";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
 
 interface IRatingInputField {
   selectedSong: SongModel;
 }
 
 const RatingInputField = (props: IRatingInputField) => {
-  const id = open ? "rating-popover" : undefined;
+
   const { userInfo, userCanReview } = useUserState();
-  const dispatch = useSongDispatch();
   const [isEditing, setIsEditing] = React.useState<boolean>(false);
 
   const [ratingNum, setRatingNum] = React.useState<number | null>(null);
@@ -24,64 +30,97 @@ const RatingInputField = (props: IRatingInputField) => {
     setRatingNum(props.selectedSong?.rating);
   }, [props.selectedSong]);
 
-  const handleClearField = () => {
-    if (ratingNum === null) {
-      setRatingNum(null);
-    } else {
-      setRatingNum(ratingNum)
-    };
-    setIsEditing(false)
+  const submitRating = async (toSubmit: SongModel) => {
+    try {
+      await call(SongsApi).apiSongsPut({ songModel: toSubmit });
+      setIsEditing(false);
+    } catch (e) {
+      console.log("song rating failed", e);
+    }
   };
 
-  const handleClickAway = () => {
-    handleClearField();
-    console.log("clicked away!");
+  const handleClearField = () => {
+    setRatingNum(props.selectedSong?.rating);
+    setIsEditing(false);
   };
 
   return (
-    <Tooltip title={userCanReview ? "Click to Rate" : ''} sx={{ cursor: userCanReview ? "pointer" : "unset"}}>
-      {
-        isEditing ?
-          <ClickAwayListener onClickAway={handleClickAway}>
+    <Tooltip title={userCanReview && !isEditing ? "Click to Rate" : ""}>
+      <Box
+        component="span"
+        sx={{ cursor: userCanReview ? "pointer" : "unset" }}
+      >
+        {isEditing ? (
+          <ClickAwayListener onClickAway={handleClearField}>
             <Grid container alignItems="center" direction="row">
               <Grid item>
                 <TextField
                   autoFocus
                   sx={{ width: "70px" }}
-                  inputProps={{ min: 0, max: 10, style: { fontSize: "1.25rem" } }}
+                  inputProps={{
+                    min: 0,
+                    max: 10,
+                    style: { fontSize: "1.25rem" },
+                  }}
                   type="number"
                   variant="standard"
                   value={ratingNum}
                   onChange={(e) => setRatingNum(Number(e.target.value))}
                 />
-              </Grid >
-              <Stack direction="column" alignContent="center" justifyContent="center" sx={{ marginLeft: '10px', transform: 'scale(0.9)' }}>
-                <Grid item >
-                  <IconButton color="secondary" onClick={(e) => {
-                    dispatch({
-                      type: "receiveSongEvent",
-                      payload: { ...props.selectedSong, rating: ratingNum, reviewer: { ...userInfo } },
-                    })
-                    setIsEditing(false);
-                  }
-                  } sx={{ marginLeft: "auto", maxHeight: "1px", maxWidth: "1px", marginBottom: "12px" }}>
+              </Grid>
+              <Stack
+                direction="column"
+                alignContent="center"
+                justifyContent="center"
+                sx={{ marginLeft: "10px", transform: "scale(0.9)" }}
+              >
+                <Grid item>
+                  <IconButton
+                    color="secondary"
+                    onClick={(e) => {
+                      submitRating({
+                        ...props.selectedSong,
+                        rating: ratingNum,
+                        reviewer: { ...userInfo },
+                      });
+                    }}
+                    sx={{
+                      marginLeft: "auto",
+                      maxHeight: "1px",
+                      maxWidth: "1px",
+                      marginBottom: "12px",
+                    }}
+                  >
                     <CheckIcon />
                   </IconButton>
                 </Grid>
                 <Grid item>
                   <IconButton
                     onClick={handleClearField}
-                    sx={{ marginLeft: "auto", maxHeight: "1px", maxWidth: "1px", }}
+                    sx={{
+                      marginLeft: "auto",
+                      maxHeight: "1px",
+                      maxWidth: "1px",
+                    }}
                     color="primary"
                   >
                     <CloseIcon />
                   </IconButton>
                 </Grid>
-              </Stack >
-            </Grid >
-          </ClickAwayListener> : <Typography onClick={() => userCanReview ? setIsEditing(true) : {}} variant="h6" color={"secondary.main"} alignContent="center">
-            {props.selectedSong?.rating ?? "--"}</Typography>
-      }
+              </Stack>
+            </Grid>
+          </ClickAwayListener>
+        ) : (
+          <Typography
+            onClick={() => (userCanReview ? setIsEditing(true) : {})}
+            variant="h6"
+            color="secondary.main"
+            alignContent="center"
+          >
+            {props.selectedSong?.rating ?? "--"}
+          </Typography>
+        )}
+      </Box>
     </Tooltip>
   );
 };
